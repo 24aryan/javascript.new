3757b88ea38be7a5307dc8d400268fd4fd053805
Mishra Aryan <aryanmishra570@gmail.com>
2024-02-14 16:21:28 +0530

    Add files via upload

---
+++ 03_operations(2).js
@@ -0,0 +1,36 @@
+let value=3;
+let negvalue=-value
+//console.log(negvalue);
+// TO GIVE NEGATIVE 
+
+// console.log(2+2)
+// console.log(2-2)
+// two multiply for raise to power
+// console.log(2**2)
+// modulo to find remainder
+// console.log(2%2)
+
+let str1="hello"
+let str2=" Aryan"
+let str3= str1 + str2
+// console.log(str3);
+// all this are just simple operations
+
+
+// now in this operation a string is being added to a number  which is giving o/p 12 
+// console.log( "1"+2);
+// console.log("1"+2+2)
+// console.log(2+2+"1")
+// here in abve two the o/p is dependent upon the first type of declarement 
+// if we give string first operation will be treated as string only and  if number vice- versa  
+// instead use parenthesis and write clean code
+
+let gamecounter=100
+++gamecounter
+gamecounter++
+// here both of them have difference as one of them will be incermented before the process and the other one after the process 
+
+//If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+//If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+
