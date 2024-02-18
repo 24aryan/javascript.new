f9c21785649d801a6f960ef9ea6a80c0405817da
Mishra Aryan <aryanmishra570@gmail.com>
2024-02-18 22:49:14 +0530

    Add files via upload

---
+++ 05_string.js
@@ -0,0 +1,27 @@
+const name="Aryan"
+const repovalue=4
+
+// here this is the modern way to print string 
+// the ` ` is used to write and ${} in this is to print values of the variable 
+
+ console.log(`hello my name is ${name} and my repo count is ${repovalue}`);
+
+//declaring string using string builder
+const gamename=new String('AryanM')
+//by declaring in such a way string values are stored in key value format and it starts from 0
+//and with this we can use multiple methods of string
+
+// here we printing the first key value of string
+console.log(gamename[0]);
+
+// in this all the method of string exists if printed it will empty but have methods
+// console.log(gamename__proto__)
+
+// nut we can access this method and to see this methods we can call 
+//const gamename=new String('AryanM') in terminal.
+
+//example of a method to find length if string
+console.log(gamename.length);
+// similarly different methods are uppercase(),charAt(),indexof()
+
+////////////////////pending //////////////////////
