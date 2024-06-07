6643db5c96694c70593937fcd8fa906f18872553
Mishra Aryan <aryanmishra570@gmail.com>
2024-06-07 22:09:03 +0530

    Update 01_functions.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -50,4 +50,42 @@ function addtwonumber2(number1 , number2){
  console.log(justloggedin("suresh"));
 //  but if we don't pass argument it will give undefined so how to detect that if name is there or not use if else 
 
+//////////////////next lecture ///////////////////////////////////////
 
+// now assume we have a shopping cart we need to add elements and add their prices
+// but the problem is we don't know how many parameters or say items will be there 
+
+// here this function have one argument and can return only one value
+function calculatecart(num1){
+    return num1
+}
+
+// now using rest method which is same in declaration of spread
+// with rest method we can pass as many parameter we want and it will give the values in an array on which further methods can be applied
+function calculatecartprice(...num1){
+    return num1
+}
+console.log(calculatecartprice(200,400,800,450));
+
+// let us use object in function
+
+const user ={
+    name:"Aryan",
+    price:199
+}
+// creating a function and passing anyobject as paramter
+function handleobject(anyobject){
+    // now we are printing with string polarize and fetching object key value 
+    // but we need to be very specific with key name as change in it result to undefined values
+      console.log(`the user ${anyobject.name} has a bill of ${anyobject.price}`)
+}
+handleobject(user)
+// we can directly pass the value of object in parameter only 
+
+// passing array
+const myarray = [12,84,75,78]
+
+function givesecondindex(anyarray){
+    return anyarray[1]
+}
+console.log(givesecondindex(myarray))
