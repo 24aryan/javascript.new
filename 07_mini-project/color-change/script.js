7ad0fd6b45395dca293d03dbd00557785a608f1e
Mishra Aryan <aryanmishra570@gmail.com>
2024-08-10 20:43:41 +0530

    Add files via upload

---
+++ 07_mini-project/color-change/script.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll('.buttons')
+const body = document.querySelector('body')
+
+buttons.forEach(function (buttons){
+    console.log(buttons);
+    buttons.addEventListener('click' , function (event) {
+        console.log(event);
+        console.log(event.target);
+        
+        if(event.target.id === 'grey'){
+            body.style.backgroundColor = event.target.id;
+        }
+        if(event.target.id === 'yellow'){
+            body.style.backgroundColor = event.target.id;
+        }
+        if(event.target.id === 'green'){
+            body.style.backgroundColor = event.target.id;
+        }
+        if(event.target.id === 'red'){
+            body.style.backgroundColor = event.target.id;
+        }
+         
+    })
+})
